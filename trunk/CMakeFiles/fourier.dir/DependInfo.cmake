608a5469aca5a8b81cd756b8ef0246b0b340b41e
louachenifarida@gmail.com <louachenifarida@gmail.com@21737640-e042-816c-26f0-745b194f5a1f>
2014-04-03 10:25:32 +0000

    initial import

---
+++ trunk/CMakeFiles/fourier.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dida/OpenCVTP/Fourier/fourier.cpp" "/home/dida/OpenCVTP/Fourier/CMakeFiles/fourier.dir/fourier.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
